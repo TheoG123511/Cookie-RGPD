8a89d53edc591b7bea1551c66225aa0d0888033f
Théo Gaillard <ptitteam@hotmail.fr>
2022-12-09 15:33:48 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cookie RGPD
+
+
+## How to use ?
+
+- Run example.html
+
+>> This code run only on server, because i use iframe tag.
+
+### Allow iframe (on nginx) :
+
+```
+add_header X-Frame-Options SAMEORIGIN;
+```
---
+++ cookie.js
@@ -0,0 +1,44 @@
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function cookieConsent() {
+    if (!getCookie('_consents')) {
+        $('.toast').toast('show')
+    }
+}
+function reload() {
+    window.parent.location.reload();
+}
+$('#btnDeny').click(()=>{
+    setCookie('_consents','0',7)
+    $('.toast').toast('hide')
+    reload()
+})
+
+$('#btnAccept').click(()=>{
+    setCookie('_consents','1',7)
+    $('.toast').toast('hide')
+    reload()
+})
+cookieConsent()
